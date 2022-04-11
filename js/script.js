c13976ce8066836b648ef3c9874d7aff6cb7e716
giuseppecantafio <giuseppecantafio@hotmail.it>
2022-04-11 20:11:04 +0200

    migliorato js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+/*
+html da generare tramite js
+
+<div class="container pt-5">
+                <div class="row gy-5">
+                    <div class="col-3">
+                        <div class="card d-flex justify-content-center rounded-pill">
+                            <div class="card-wrapper">
+                                <i class="fa-solid fa-fish"></i>
+                                <span class="d-block">fish</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+*/
 
 // funzione per generare numeri casuali
 
@@ -131,47 +147,72 @@ const cards = [
     const rowElm = document.createElement('div');
     rowElm.classList.add('row', 'gy-5');
     
-    cards.forEach((element)=> {
-        const colElm = document.createElement('div');
-        colElm.classList.add('col-3');
-        const cardElm= document.createElement('div');
-        cardElm.classList.add('card', 'd-flex', 'justify-content-center', 'rounded-pill');
-        const cardWrapperElm= document.createElement('div');
-        cardWrapperElm.classList.add('card-wrapper');
-        const i = document.createElement('i');
-        i.classList.add(`${element.prefix}solid`, `${element.prefix}${element.name}`);
-        i.style.color = `${element.color}`
-        i.setAttribute('alt', `${element.type}`);
-        const spanElm = document.createElement('span');
-        spanElm.classList.add('d-block');
-        spanElm.textContent = `${element.name}`;
+    // cards.forEach((element)=> {
+    //     const colElm = document.createElement('div');
+    //     colElm.classList.add('col-3');
+    //     const cardElm= document.createElement('div');
+    //     cardElm.classList.add('card', 'd-flex', 'justify-content-center', 'rounded-pill');
+    //     const cardWrapperElm= document.createElement('div');
+    //     cardWrapperElm.classList.add('card-wrapper');
+    //     const i = document.createElement('i');
+    //     i.classList.add(`${element.prefix}solid`, `${element.prefix}${element.name}`);
+    //     i.style.color = `${element.color}`
+    //     i.setAttribute('alt', `${element.type}`);
+    //     const spanElm = document.createElement('span');
+    //     spanElm.classList.add('d-block');
+    //     spanElm.textContent = `${element.name}`;
 
-        cardWrapperElm.append(spanElm)
-        cardWrapperElm.append(i)
-        cardElm.append(cardWrapperElm)
-        colElm.append(cardElm);
-        rowElm.append(colElm);
-        containerElm.append(rowElm);
-        appElm.append(containerElm);    
+    //     cardWrapperElm.append(i)
+    //     cardWrapperElm.append(spanElm)
+    //     cardElm.append(cardWrapperElm)
+    //     colElm.append(cardElm);
+    //     rowElm.append(colElm);
+    //     containerElm.append(rowElm);
+    //     appElm.append(containerElm);    
         
-    })
+    // })
 
-    /*
-html da generare tramite js
-
-<div class="container pt-5">
-                <div class="row gy-5">
-                    <div class="col-3">
-                        <div class="card d-flex justify-content-center rounded-pill">
-                            <div class="card-wrapper">
-                                <i class="fa-solid fa-fish"></i>
-                                <span class="d-block">fish</span>
-                            </div>
-                        </div>
+        function creaCards(cards) {
+            let card = '';
+            cards.forEach((element)=> {
+            card += `
+            <div class="col-3">
+                <div class="card d-flex justify-content-center rounded-pill">
+                    <div class="card-wrapper">
+                        <i class="${element.prefix}solid ${element.prefix}${element.name}" alt="${element.type}" style="color: ${element.color}"></i>
+                        <span class="d-block">${element.name}</span>
                     </div>
                 </div>
             </div>
-*/
+            `;
+            return card;
+        })
+         rowElm.innerHTML += card;
+    };
 
+    creaCards(cards);
 
+    let selezione = document.getElementById('scelta');
+
+    selezione.addEventListener("change", function(){
+
+	let tipo = selezione.value;
+
+	if(tipo === "animal"){
+		rowElm.innerHTML = "";
+		creaCards(cards.filter((element) => element.type === "animal"));
+    }else if(tipo === "all"){
+		rowElm.innerHTML = "";
+		creaCards(cards);
+	} else if (tipo === "vegetable"){
+		rowElm.innerHTML = "";
+		creaCards(cards.filter((element) => element.type === "vegetable"));
+	}else if(tipo === "user"){
+		rowElm.innerHTML = "";
+		creaCards(cards.filter((element) => element.type === "user"));
+}})
+
+    containerElm.append(rowElm);
+    appElm.append(containerElm);   
+    
 })();
