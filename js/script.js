12e167fbfc6760ab753a5afc764b02d9d7784376
giuseppecantafio <giuseppecantafio@hotmail.it>
2022-04-11 16:57:18 +0200

    aggiunte card tramite js

--- js/script.js
+++ js/script.js
@@ -124,7 +124,39 @@ const cards = [
 	}
 ];
 
-/*
+(function (){
+    const appElm = document.getElementById('app');
+    const containerElm = document.createElement('div');
+    containerElm.classList.add('container', 'pt-5');
+    const rowElm = document.createElement('div');
+    rowElm.classList.add('row', 'gy-5');
+    
+    cards.forEach((element)=> {
+        const colElm = document.createElement('div');
+        colElm.classList.add('col-3');
+        const cardElm= document.createElement('div');
+        cardElm.classList.add('card', 'd-flex', 'justify-content-center', 'rounded-pill');
+        const cardWrapperElm= document.createElement('div');
+        cardWrapperElm.classList.add('card-wrapper');
+        const i = document.createElement('i');
+        i.classList.add(`${element.prefix}solid`, `${element.prefix}${element.name}`);
+        i.style.color = `${element.color}`
+        i.setAttribute('alt', `${element.type}`);
+        const spanElm = document.createElement('span');
+        spanElm.classList.add('d-block');
+        spanElm.textContent = `${element.name}`;
+
+        cardWrapperElm.append(spanElm)
+        cardWrapperElm.append(i)
+        cardElm.append(cardWrapperElm)
+        colElm.append(cardElm);
+        rowElm.append(colElm);
+        containerElm.append(rowElm);
+        appElm.append(containerElm);    
+        
+    })
+
+    /*
 html da generare tramite js
 
 <div class="container pt-5">
@@ -141,9 +173,5 @@ html da generare tramite js
             </div>
 */
 
-(function (){
-    const appElm = document.getElementById('app');
-    const containerElm = document.createElement('div');
-    containerElm.classList.add('container', 'pt-5');
-    console.log(containerElm)
+
 })();
